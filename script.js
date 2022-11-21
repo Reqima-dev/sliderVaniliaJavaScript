b7a95e3584d56f7c4ad03a57f32d570197e5bf85
Reqima-dev <adloukondo@gmail.com>
2022-11-21 10:34:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const sliderInit = function (currentSlider) {
+  const sliderContainer = currentSlider.querySelector(
+    "[data-slider-container]"
+  );
+  const sliderPrevBtn = currentSlider.querySelector(" [data-slider-prev] ");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+  const totalSliderVisibleItems = Number(
+    getComputedStyle(currentSlider).getPropertyValue("--slider-items")
+  );
+
+  const totlaSliderItems =
+    sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  let currentSlidePos = 0;
+
+  /*------- NEXT SLIDE -------*/
+  const slideNext = function () {
+    currentSlidePos++;
+
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+
+    if (currentSlidePos >= totlaSliderItems)
+      sliderNextBtn.setAttribute("disabled", "");
+    sliderPrevBtn.removeAttribute("disabled");
+  };
+
+  sliderNextBtn.addEventListener("click", slideNext);
+
+  /*------- Prev SLIDE -------*/
+  const slidePrev = function () {
+    currentSlidePos--;
+
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+
+    if (currentSlidePos <= 0) sliderPrevBtn.setAttribute("disabled", "");
+    sliderNextBtn.removeAttribute("disabled");
+  };
+
+  sliderPrevBtn.addEventListener("click", slidePrev);
+
+  const dontHaveExtraItem = totlaSliderItems <= 0;
+  if (dontHaveExtraItem) sliderNextBtn.setAttribute("disabled", "");
+  sliderPrevBtn.setAttribute("disabled", "")
+};
+
+for (let i = 0, len = sliders.length; i < len; i++) {
+  sliderInit(sliders[i]);
+}
